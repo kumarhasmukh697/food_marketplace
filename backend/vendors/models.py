24f9c133901d05c286d41cd7ecbdab7afa8d6077
kumarhasmukh697 <kumarhasmukh697@gmail.com>
2026-07-02 11:30:39 +0530

    alter the vendor model removed phone number and address field and linked vendor model to address model via foregin key

--- backend/vendors/models.py
+++ backend/vendors/models.py
@@ -1,17 +1,14 @@
 from django.conf import settings
 from django.db import models
+from accounts.models import Address
 
-
-# get the user model from settings
-user = settings.AUTH_USER_MODEL
-
+User = settings.AUTH_USER_MODEL
 
 class Vendor(models.Model):
-    user = models.OneToOneField(user,on_delete=models.CASCADE,related_name="vendor_profile",)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="vendor_profile",)
     shop_name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
-    address = models.CharField(max_length=255, blank=True)
+    address = models.ForeignKey(Address,on_delete=models.SET_NULL,null=True,blank=True,related_name="vendors",)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
