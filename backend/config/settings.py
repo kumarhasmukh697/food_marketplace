08c814a8fe76d2cd98a4e8f138dda9c3437779bc
kumarhasmukh697 <kumarhasmukh697@gmail.com>
2026-09-24 21:27:05 +0530

    talwind production css problem fixed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+
 
 
 def get_env_list(name, default=""):
